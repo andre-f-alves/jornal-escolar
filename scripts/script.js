c8ff730fe3ef6cb50f471f9ee860cc8f8e712409
André Alves <112015245+andre-f-alves@users.noreply.github.com>
2023-09-04 18:50:14 -0300

    navigation system

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,23 @@
 const menuIcon = document.querySelector('.menu-icon')
 const navList = document.querySelector('.nav-list')
+const navLinks = document.querySelectorAll('.nav-list a')
+const htmlSections = document.querySelectorAll('section')
+
+const sections = [ ...htmlSections ].map(section => ({ [section.getAttribute('id')]: section.offsetTop }))
+
+const linksArray = [ ...navLinks ]
+
+linksArray.forEach((link, index)=> {
+  link.addEventListener('click', event => {
+    event.preventDefault()
+
+    const elementId = event.target.getAttribute('href').substring(1)
+    scrollTo({
+      top: sections[index][elementId] - 20,
+      behavior: 'smooth'
+    })
+  })
+})
 
 menuIcon.addEventListener('click', () => {
   navList.classList.toggle('active')
